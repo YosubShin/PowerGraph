58880e2108a12eebfba2cf0761157b3a95a5f871
Yosub Shin <yosub@yosubshin.com>
2015-10-12 13:48:56 -0500

    got rid of source list from approximate_diameter.py

--- approximate_diameter.py
+++ approximate_diameter.py
@@ -18,12 +18,9 @@ hostfile_path = '%s/machines' % home_path
 
 for run in range(3):  # Do experiment X many times
     for tolerance in tolerances:
-        lines = subprocess.check_output(['shuf', '-n', str(tolerance), '%s/lab_data_lj' % home_path])
-        lines = lines.split('\n')
-        source_list = map(lambda l: '--source=' + l.split('\t')[0], filter(lambda x: len(x) > 0, lines))
         for algorithm in algorithms:
             algorithm_path = '%s/%s' % (graph_analytics_bin_path, algorithm)
             for partitioning_strategy in partitioning_strategies:
                 print('algorithm: %s, partitioning_strategy: %s, tolerance: %d' % (algorithm, partitioning_strategy, tolerance))
-                check_call(['mpiexec', '-n', str(num_hosts), '-hostfile', hostfile_path, algorithm_path, '--graph=%s' % graph_path, '--format=snap', '--graph_opts=ingress=%s' % partitioning_strategy, '--tol=%d' % tolerance] + source_list,
+                check_call(['mpiexec', '-n', str(num_hosts), '-hostfile', hostfile_path, algorithm_path, '--graph=%s' % graph_path, '--format=snap', '--graph_opts=ingress=%s' % partitioning_strategy, '--tol=%d' % tolerance],
                            stderr=STDOUT) 
