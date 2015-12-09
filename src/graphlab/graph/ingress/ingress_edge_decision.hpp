b2c37480d8923212a92979b7ca31763c81baaf09
Yosub Shin <yosub@yosubshin.com>
2015-12-08 23:47:21 -0600

    optimize parameter for ingress_edge_decision

--- src/graphlab/graph/ingress/ingress_edge_decision.hpp
+++ src/graphlab/graph/ingress/ingress_edge_decision.hpp
@@ -172,7 +172,8 @@ namespace graphlab {
              // size_t src_dist = coords_pair_dist(src_coord, dc_.topologies()[i]);
              // size_t dst_dist = coords_pair_dist(dst_coord, dc_.topologies()[i]);
              double bal = (maxedges - proc_num_edges[i])/(epsilon + maxedges - minedges);
-             proc_score[i] = bal + ((sd > 0) + (td > 0)); // original terms (load balance + greedy)
+             proc_score[i] = bal + ((sd > 0) + (td > 0)) // original terms (load balance + greedy)
+	       + src_coord + dst_coord;
 	       // + (2.0 * shortest_dist - (src_dist + dst_dist)) / 40.0 * (2.0 * epsilon + shortest_dist) // minimize src_dist + dst_dist
                //       + (shortest_dist - std::abs(src_dist - dst_dist)) / 40.0 * (2.0 * epsilon + shortest_dist); // minimize src_dist and dst_dist difference
          }
